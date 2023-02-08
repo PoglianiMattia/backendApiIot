6d44e32f5c0688d27192d0ae377f633f095e88a6
mattia.pogliani <mattia.pogliani@gmail.com>
2023-02-08 11:40:41 +0100

    refactor code

--- ApiServer/index.js
+++ ApiServer/index.js
@@ -13,6 +13,63 @@ app.use(
 
 const connection = build_direct_mqtt();
 
+(async function connect() {
+  await connection.connect();
+})();
+
+app.listen(3000, () =>
+  console.log("server pronto sulla 3000")
+);
+
+// http://localhost:3000/luce?stato=on
+app.get("/luce", async function (req, res) {
+  console.log("in get luce", req.query.stato);
+  let res = await sendMessage(req.query.stato, "led");
+  if (res) res.send("messaggio inviato");
+  else res.send("error: " + res);
+});
+
+async function sendMessage(message, topic) {
+  try {
+    //const decoder = new TextDecoder("utf8");
+    // const on_publish = async (
+    //   topic,
+    //   payload,
+    //   dup,
+    //   qos,
+    //   retain
+    // ) => {
+    //   const json = decoder.decode(payload);
+    //   console.log(
+    //     `Publish received. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`
+    //   );
+    //   console.log(json);
+    //   const message = JSON.parse(json);
+    //   if (message.sequence == argv.count) {
+    //     resolve();
+    //   }
+    // };
+
+    // await connection.subscribe(
+    //   argv.topic,
+    //   mqtt.QoS.AtLeastOnce,
+    //   on_publish
+    // );
+
+    (async () => {
+      connection.publish(
+        topic,
+        // JSON.stringify(message),
+        message,
+        mqtt.QoS.AtLeastOnce
+      );
+    })();
+    return true;
+  } catch (error) {
+    return error;
+  }
+}
+
 function build_direct_mqtt() {
   let config_builder =
     iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(
@@ -27,10 +84,6 @@ function build_direct_mqtt() {
 
   config_builder.with_clean_session(false);
   config_builder.with_client_id("clientPogliani");
-  //   config_builder.with_client_id(
-  //     argv.client_id ||
-  //       "test-" + Math.floor(Math.random() * 100000000)
-  //   );
   config_builder.with_endpoint(
     "a2kur01j8rc1su-ats.iot.eu-central-1.amazonaws.com"
   );
@@ -38,62 +91,4 @@ function build_direct_mqtt() {
 
   const client = new mqtt.MqttClient();
   return client.new_connection(config);
-}
-
-(async function connect() {
-  await connection.connect();
-})();
-
-//await connection.disconnect();
-app.listen(3000, () =>
-  console.log("server pronto sulla 3000")
-);
-
-// http://localhost:3000/luce?stato=on
-app.get("/luce", async function (req, res) {
-  console.log("in get luce", req.query.stato);
-  await sendMessage(req.query.stato, "led");
-  //res.send();
-});
-0;
-async function sendMessage(message, topic) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      //const decoder = new TextDecoder("utf8");
-      // const on_publish = async (
-      //   topic,
-      //   payload,
-      //   dup,
-      //   qos,
-      //   retain
-      // ) => {
-      //   const json = decoder.decode(payload);
-      //   console.log(
-      //     `Publish received. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`
-      //   );
-      //   console.log(json);
-      //   const message = JSON.parse(json);
-      //   if (message.sequence == argv.count) {
-      //     resolve();
-      //   }
-      // };
-
-      // await connection.subscribe(
-      //   argv.topic,
-      //   mqtt.QoS.AtLeastOnce,
-      //   on_publish
-      // );
-
-      (async () => {
-        connection.publish(
-          topic,
-          // JSON.stringify(message),
-          message,
-          mqtt.QoS.AtLeastOnce
-        );
-      })();
-    } catch (error) {
-      reject(error);
-    }
-  });
 }
